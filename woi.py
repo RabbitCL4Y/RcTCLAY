0549fd5fc695dc22870a27af69ef633b8aa47294
Muhammad Rafli <44805762+RabbitCL4Y@users.noreply.github.com>
2019-01-20 13:38:18 +0800

    Create woi.py

---
+++ woi.py
@@ -0,0 +1,16 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+mengetik('EXIT IN MY TOOLS')
+mengetik('Follow Me On The Social Media')
+mengetik('My Github     : https://github.com/RabbitCL4Y')
+mengetik('My Instagram  : https://www.instagram.com/muhammadrafli_337')
+mengetik('My Channel Yt : Rabbit Cybet Team')
+mengetik('Contact Me    : RabbitCL4Y123@gmail.com')
+mengetik('Every successful person must have a failure. Do not be afraid to fail because failureis a part of success')
+
